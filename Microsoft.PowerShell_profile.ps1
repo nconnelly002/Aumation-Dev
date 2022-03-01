4132e3a446da473a1c0dc132e7c860affd71f089
nconnelly002 <100610949+nconnelly002@users.noreply.github.com>
2022-03-01 09:54:46 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,9 +7,6 @@ write-host
 write-host "Upgrade-profile = updates powershell profile"
 
 set-alias -name sync -Value 'S:\IT Department\Automation\ADSynce.ps1'
-set-alias -name new-hire -Value 'S:\IT Department\Automation\Create_users.ps1'
 set-alias -Name ifconfig ipconfig
-Set-Alias -name manage-license -Value 'S:\IT Department\Automation\manage-officelicenses.ps1'
-Set-Alias -name exchange -Value 'S:\IT Department\Automation\Connect_to_exchange.ps1' 
-Set-Alias -Name remove-user -value 'S:\IT Department\Automation\remove-user.ps1'
 set-alias -name application-services -Value 'S:\IT Department\Automation\Network_services.ps1'
+set-alias -name upgrade-profile -Value 'S:\IT Department\Automation\Push_Profile.ps1'
