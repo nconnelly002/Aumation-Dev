423f0a5b338519bdd133114ed0d07fff6c757e3c
nconnelly002 <100610949+nconnelly002@users.noreply.github.com>
2022-03-01 12:06:26 -0500

    Update retentionpolicy.ps1

--- retentionpolicy.ps1
+++ retentionpolicy.ps1
@@ -5,13 +5,14 @@ $falseFiles
 $daysback = -2555
 $CurrentDate = Get-Date
 $DatetoDelete = $CurrentDate.AddDays($DaysBack)
-$oldest = 'Monday, ‎December ‎31, ‎1979, ‏‎11:00:00 PM'
+
+
 
 $falseFiles = Get-ChildItem $location -Recurse | Where-Object {$_.lastwritetime -like '*1979*'}
 foreach($falseFile in $falseFiles){ $falseFile.LastWriteTime = (get-date)}
 $files = Get-ChildItem $location -Recurse
-$deleted =Get-ChildItem $location -Recurse { $_.LastWriteTIme -lt $DatetoDelete}
+$deleted =Get-ChildItem $location -Recurse { $_.LastWriteTIme -is $DatetoDelete}
 write-host "Deleted items" 
 write-host $deleted
-$files | Where-Object { $_.LastWriteTIme -lt $DatetoDelete -and $_.LastWriteTime -isnot $oldest} | Remove-Item -Recurse -ErrorAction Ignore
+$files | Where-Object { $_.LastWriteTIme -lt $DatetoDelete} | Remove-Item -Recurse -ErrorAction Ignore
 
