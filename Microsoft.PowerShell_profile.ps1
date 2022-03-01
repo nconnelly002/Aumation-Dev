86f9c2d9690033018248e2cfe580bf33c1476e2b
nconnelly002 <100610949+nconnelly002@users.noreply.github.com>
2022-03-01 09:51:06 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,8 @@ Write-host "Welcome" $env:UserName -ForegroundColor Blue
 Write-host "sync = Is a custom command for Edelstein to Sync the local Active Directory with Office 365"-ForegroundColor Green
 write-host 
 write-host 'application-services = Custom tool to check various application services and Office 365 Licensing' -ForegroundColor Green
+write-host 
+write-host "Upgrade-profile = updates powershell profile"
 
 set-alias -name sync -Value 'S:\IT Department\Automation\ADSynce.ps1'
 set-alias -name new-hire -Value 'S:\IT Department\Automation\Create_users.ps1'
